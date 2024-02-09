eab3c4c5bebdcd2f294d1d1340cf6e95c9e7f867
Jordan Herzstein <jordanherzstein@gmail.com>
2024-02-09 16:28:28 -0500

    updating for hugo

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+hugo 
+read -r -p "Do you want to sync changes to the server and update git? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    neocities push ~/Website/public/
+    git add .
+    git commit -m "auto updating site"
+    git push origin master
+    git switch gh-pages
+    rm -r *
+    git checkout master -- ~/Website/public/
+    mv public/* .
+    rm -r public/
+    git add .
+    git commit -m "updating for hugo"
+    git push origin gh-pages
+    git switch master
+else
+    exit 0
+fi
